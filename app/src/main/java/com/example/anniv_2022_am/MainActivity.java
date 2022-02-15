047f5a1800c9b2ef59563888e48bf4a2645f851d
Raphael <raphikikiki@gmail.com>
2022-02-15 10:28:07 +0100

    Abandon des notifications => manque de temps

--- app/src/main/java/com/example/anniv_2022_am/MainActivity.java
+++ app/src/main/java/com/example/anniv_2022_am/MainActivity.java
@@ -1,15 +1,12 @@
 package com.example.anniv_2022_am;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.NotificationCompat;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
-import android.app.NotificationChannel;
-import android.app.NotificationManager;
-import android.os.Build;
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
@@ -29,37 +26,6 @@ public class MainActivity extends AppCompatActivity {
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupWithNavController(navView, navController);
-
-        NotificationManager notificationManager = this.createNotificationChannel();
-
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, "0")
-                .setSmallIcon(R.drawable.ic_launcher_foreground)
-                .setContentTitle("My notification")
-                .setContentText("Hello World!")
-                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
-                // Set the intent that will fire when the user taps the notification
-                .setAutoCancel(true);
-
-        notificationManager.notify(0, builder.build());
     }
 
-    private NotificationManager createNotificationChannel() {
-        NotificationManager notificationManager;
-        // Create the NotificationChannel, but only on API 26+ because
-        // the NotificationChannel class is new and not in the support library
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            //CharSequence name = getString(R.string.channel_name);
-            //String description = getString(R.string.channel_description);
-            int importance = NotificationManager.IMPORTANCE_DEFAULT;
-            NotificationChannel channel = new NotificationChannel("0", "Name", importance);
-            channel.setDescription("Une description");
-            // Register the channel with the system; you can't change the importance
-            // or other notification behaviors after this
-            notificationManager = getSystemService(NotificationManager.class);
-            notificationManager.createNotificationChannel(channel);
-        } else {
-            notificationManager = (NotificationManager)getSystemService(this.getApplicationContext().NOTIFICATION_SERVICE);
-        }
-        return notificationManager;
-    }
 }
